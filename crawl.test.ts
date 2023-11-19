d0ab633a4fc92f0995b029e677e6ac3dc6022a2c
Leonardo Gatti <leonardogattidev@gmail.com>
2023-11-19 02:45:47 -0300

    added getURLsFromHTML function+tests

--- crawl.test.ts
+++ crawl.test.ts
@@ -1,5 +1,80 @@
 import { describe, expect, test } from "vitest";
-import { normalizeURL } from "./crawl";
+import { getURLsFromHTML, normalizeURL } from "./crawl";
+
+describe("getURLsFromHTML", () => {
+  test("absolute URLs", () => {
+    const input = {
+      htmlBody: `
+  <html>
+    <body>
+      <a href="https://leonardo-gatti.pages.dev">
+        Leonardo Gatti Website
+      </a>
+    </body>
+  <html>
+        `,
+      baseURL: `https://leonardo-gatti.pages.dev`,
+    };
+    const actual = getURLsFromHTML(input.htmlBody, input.baseURL);
+    const expected = ["https://leonardo-gatti.pages.dev/"];
+    expect(actual).toEqual(expected);
+  });
+
+  test("relative URLs", () => {
+    const input = {
+      htmlBody: `
+  <html>
+    <body>
+      <a href="/resume.pdf">
+        Leonardo Gatti Website
+      </a>
+    </body>
+  <html>
+        `,
+      baseURL: `https://leonardo-gatti.pages.dev`,
+    };
+    const actual = getURLsFromHTML(input.htmlBody, input.baseURL);
+    const expected = ["https://leonardo-gatti.pages.dev/resume.pdf"];
+    expect(actual).toEqual(expected);
+  });
+
+  test("multiple URLs", () => {
+    const input = {
+      htmlBody: `
+<html>
+  <body>
+    <a href="#hero">
+      absolute resume
+    </a>
+    <a href="/resume.pdf">
+      absolute resume
+    </a>
+    <a href="resume.pdf">
+      relative resume
+    </a>
+    <a href="https://leonardo-gatti.pages.dev">
+      Leonardo Gatti Website
+    </a>
+    <a href="https://google.com">
+      Google
+    </a>
+
+  </body>
+<html>
+      `,
+      baseURL: `https://leonardo-gatti.pages.dev`,
+    };
+    const actual = getURLsFromHTML(input.htmlBody, input.baseURL);
+    const expected = [
+      "https://leonardo-gatti.pages.dev/#hero",
+      "https://leonardo-gatti.pages.dev/resume.pdf",
+      "https://leonardo-gatti.pages.dev/resume.pdf",
+      "https://leonardo-gatti.pages.dev/",
+      "https://google.com/",
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
 
 describe("normalizeURL", () => {
   test("strip protocol", () => {
--- crawl.ts
+++ crawl.ts
@@ -1,3 +1,19 @@
+import { Window } from "happy-dom";
+
+export function getURLsFromHTML(htmlBody: any, baseURL: any): string[] {
+  const urls: string[] = [];
+  const window = new Window();
+  const document = window.document;
+  document.write(htmlBody);
+  const linkElements = document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    const href = linkElement.getAttribute("href");
+    const url = new URL(href, baseURL);
+    urls.push(url.toString());
+  }
+  return urls;
+}
+
 export function normalizeURL(urlString: string) {
   const urlObj = new URL(urlString);
   let url = urlObj.hostname + urlObj.pathname;
